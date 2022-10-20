12f555541ce33facbedb4f0a0f758f0d74ee1680
Sergio Giménez <sergiogimenezanton@gmail.com>
2022-10-20 15:53:57 +0200

    Added one init script for master and worker node

---
+++ demo/init_master.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+####################################################################
+# Assumption: k8s/kubeadm was deployed using podcidr=10.240.0.0/16 #
+####################################################################
+
+# Allow pod to pod communication
+iptables -A FORWARD -s 10.240.0.0/16 -j ACCEPT
+iptables -A FORWARD -d 10.240.0.0/16 -j ACCEPT
+
+# Allow communication across hosts (from master <--> PodCidr of worker node)
+ip route add 10.240.1.0/24 via 10.10.0.11 dev ens4
+
+# Allow outgoing internet
+iptables -t nat -A POSTROUTING -s 10.240.0.0/24 ! -o cni0 -j MASQUERADE
