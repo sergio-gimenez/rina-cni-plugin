e39802b2727fdab5aade3cb495399b29249e59f8
Sergio Giménez <sergiogimenezanton@gmail.com>
2022-10-24 15:47:19 +0200

    Assert scripts are executed as root

--- demo/init_worker.sh
+++ demo/init_worker.sh
@@ -4,6 +4,13 @@
 # Assumption: k8s/kubeadm was deployed using podcidr=10.240.0.0/16 #
 ####################################################################
 
+# display usage if the script is not run as root user
+if [[ "$EUID" -ne 0 ]]; then
+    echo "This script must be run as root!"
+    exit 1
+fi
+
+
 # Allow pod to pod communication
 iptables -A FORWARD -s 10.240.0.0/16 -j ACCEPT
 iptables -A FORWARD -d 10.240.0.0/16 -j ACCEPT
