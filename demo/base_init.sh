601fbaa45cdcf76c114f8f74fd9d9e68d4d06cca
Sergio Giménez <sergiogimenezanton@gmail.com>
2022-11-23 19:00:28 +0100

    Removed the internet thing because cni0 does not exist in RINA

--- demo/base_init.sh
+++ demo/base_init.sh
@@ -14,5 +14,6 @@ fi
 iptables -A FORWARD -s 10.240.0.0/16 -j ACCEPT
 iptables -A FORWARD -d 10.240.0.0/16 -j ACCEPT
 
+# TODO Test internet access from pods
 # Allow outgoing internet
-iptables -t nat -A POSTROUTING -s 10.240.0.0/24 ! -o cni0 -j MASQUERADE
+# iptables -t nat -A POSTROUTING -s 10.240.0.0/24 ! -o cni0 -j MASQUERADE
